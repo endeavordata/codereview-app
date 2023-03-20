c06eaa20de54a9c9f1b169090533db4b880d194d
Nathan Gould <nathan.gould@gmail.com>
2023-03-20 18:10:50 +0530

    feat: add codereview header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,31 +1,81 @@
-import * as React from 'react';
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
 
-import UnstyledLink from '@/components/links/UnstyledLink';
+import TCRLogo from '../../..//public/images/tcrlogo.png'
 
-const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
-];
+const navigation = [
+  { name: 'Dashboards', href: '#', current: true },
+  { name: 'Blog', href: '#', current: false },
+  { name: 'About', href: '#', current: false },
+]
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
-        </UnstyledLink>
-        <nav>
-          <ul className='flex items-center justify-between space-x-4'>
-            {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
-                  {label}
-                </UnstyledLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
+    <div className='min-h-full'>
+      <Disclosure
+        as='nav'
+        className='border-b border-gray-200 bg-white py-2 pt-3'
+      >
+        {({ open }) => (
+          <>
+            <div className='max-w-8xl mx-auto px-6 sm:px-8 lg:px-12'>
+              <div className='flex h-16 justify-between'>
+                <div className='flex flex-shrink-0 items-center'>
+                  <Image alt='The Code Review logo' src={TCRLogo} width={240} />
+                </div>
+                <div className='hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8'>
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={
+                        item.current
+                          ? 'text-md inline-flex items-center px-1 pt-3 font-mono font-medium text-gray-900'
+                          : 'text-md inline-flex items-center px-1 pt-3 font-mono font-medium text-gray-500 hover:text-gray-700'
+                      }
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className='-mr-2 flex items-center sm:hidden'>
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className='inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2'>
+                    <span className='sr-only'>Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className='block h-8 w-8' aria-hidden='true' />
+                    ) : (
+                      <Bars3Icon className='block h-8 w-8' aria-hidden='true' />
+                    )}
+                  </Disclosure.Button>
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className='sm:hidden'>
+              <div className='space-y-1 pt-2 pb-3'>
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as='a'
+                    href={item.href}
+                    className={
+                      item.current
+                        ? 'block border-gray-500 bg-gray-50 py-2                       pl-3 pr-4 font-mono text-base font-medium text-gray-700'
+                        : 'text-gray-60 block border-transparent                       py-2 pl-3 pr-4 font-mono text-base font-medium hover:bg-gray-50 hover:text-gray-800'
+                    }
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </div>
+  )
 }
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,14 @@
-import * as React from 'react';
+import * as React from 'react'
+
+import Header from '@/components/layout/Header'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
-  return <>{children}</>;
+  return (
+    <div>
+      <Header />
+      <div>{children}</div>
+      <div>Doop</div>
+    </div>
+  )
 }
