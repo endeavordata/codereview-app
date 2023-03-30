fb0909745783718663dfb715d98184b3297e53e7
Nathan Gould <nathan.gould@gmail.com>
2023-03-30 19:46:23 +0700

    fix: try using dynamic import in next sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,10 @@
-import { getReportPaths } from '@/lib/reports'
-
 /**
  * @type {import('next-sitemap').IConfig}
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
   additionalPaths: async () => {
+    const { getReportPaths } = await import('@/lib/reports')
     const paths = await getReportPaths()
     return paths.map((path) => ({ loc: path }))
   },
