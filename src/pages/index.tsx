55d377ee9b147ce8d024f5c9417486461cd07be6
Nathan Gould <nathan.gould@gmail.com>
2023-04-07 20:04:02 +0700

    feat: homepage copy and layout, wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,18 +34,25 @@ const HomePage = ({ topics, totalPages }: HomePageProps) => {
             />
           </div>
         </section>
+
         <section className='bg-white'>
+          <div className='mt-12 text-center'>
+            <p className='mb-2 text-2xl italic'>
+              The Code Review was created by technology bettors, for technology
+              bettors.
+            </p>
+          </div>
           <div className='layout flex flex-wrap py-12'>
-            <div className='w-full md:w-1/2'>
-              <h2 className='mb-1 text-xl font-bold'>Technology Bettor</h2>
+            <div className='w-full rounded-2xl bg-gray-100 p-12 shadow-lg md:w-1/2'>
+              <h2 className='mb-1 text-2xl font-bold'>technology bettor</h2>
               <p className='mb-1 text-lg text-gray-800'>
                 <em>/təkˈnäləjē ˈbetər/</em>
               </p>
-              <p className='mb-2 text-lg text-gray-800'>
+              <p className='mb-2 text-lg font-semibold text-gray-800'>
                 <em>noun</em>
               </p>
               <ol className='list-inside list-decimal text-gray-800'>
-                <li>
+                <li className='mb-2'>
                   A person responsible for making decisions informed by an
                   up-to-date understanding of software industry trends.
                 </li>
@@ -55,29 +62,34 @@ const HomePage = ({ topics, totalPages }: HomePageProps) => {
                 </li>
               </ol>
             </div>
-            <div className='w-full md:w-1/2'>
-              <p className='mb-2 text-lg font-bold'>
-                The CodeReview is for technology bettors like you:
-              </p>
-              <ol className='list-inside list-decimal text-gray-800'>
-                <li>
-                  Engineering leaders: Gain insights into the latest tools and
-                  technologies to help your team stay at the cutting edge.
+            <div className='w-full pl-4 md:w-1/2 md:pl-8'>
+              <p className='mb-2 text-2xl font-bold'>Are you one of us?</p>
+              <ul className='list-inside text-gray-800'>
+                <li className='mb-4'>
+                  <span className='font-semibold'>Engineering leaders:</span>{' '}
+                  Gain insights into the latest tools and technologies to help
+                  your team stay at the cutting edge.
                 </li>
-                <li>
-                  Developer marketers: Understand industry trends to better
-                  target your messaging and engage your audience.
+                <li className='mb-4'>
+                  <span className='font-semibold'>Technology investors:</span>{' '}
+                  Make informed investments based on data-driven insights into
+                  technology adoption and growth.
                 </li>
-                <li>
-                  Technology investors: Make informed investments based on
-                  data-driven insights into technology adoption and growth.
+                <li className='mb-4'>
+                  <span className='font-semibold'>
+                    Developer-focused marketers:
+                  </span>{' '}
+                  Understand industry trends to better target your messaging and
+                  engage your audience.
                 </li>
-                <li>
-                  Developer- product managers: Identify opportunities and
-                  prioritize features based on industry-wide trends and
-                  competitor analysis.
+                <li className='mb-4'>
+                  <span className='font-semibold'>
+                    Developer-focused product leaders:
+                  </span>{' '}
+                  Identify opportunities and prioritize features based on
+                  industry-wide trends and competitor analysis.
                 </li>
-              </ol>
+              </ul>
             </div>
           </div>
         </section>
