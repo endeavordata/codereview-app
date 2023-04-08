421d913c0982b7c1eb68c29fb3d6dbf48045524a
Nathan Gould <nathan.gould@gmail.com>
2023-04-08 15:33:44 +0700

    feat: more work on layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,10 +19,12 @@ const HomePage = ({ topics, totalPages }: HomePageProps) => {
       <main>
         <section className='bg-orange-100'>
           <div className='layout relative flex flex-col py-12 text-center'>
-            <h1 className='mt-16'>Software ecosystems, quantified</h1>
-            <p className='mt-6 text-lg text-gray-800'>
-              Data and insights about software, as it gets created and used.{' '}
-              <br /> Built with love for technology bettors.
+            <h1 className='mt-16 text-4xl'>Software ecosystems, quantified</h1>
+            <p className='mt-6 text-xl text-gray-800'>
+              Data and insights about software, as it gets created and used.
+            </p>
+            <p className='mt-1 text-xl text-gray-800'>
+              Built with love for technology bettors.
             </p>
           </div>
         </section>
@@ -38,12 +40,12 @@ const HomePage = ({ topics, totalPages }: HomePageProps) => {
         <section className='bg-white'>
           <div className='mt-12 text-center'>
             <p className='mb-2 text-2xl italic'>
-              The Code Review was created by technology bettors, for technology
+              The Code Review was built by technology bettors, for technology
               bettors.
             </p>
           </div>
-          <div className='layout flex flex-wrap py-12'>
-            <div className='w-full rounded-2xl bg-gray-100 p-12 shadow-lg md:w-1/2'>
+          <div className='layout flex flex-wrap justify-end py-12'>
+            <div className='w-full rounded-2xl bg-gray-100 py-16 pl-12 pr-10 shadow-lg md:w-1/2'>
               <h2 className='mb-1 text-2xl font-bold'>technology bettor</h2>
               <p className='mb-1 text-lg text-gray-800'>
                 <em>/təkˈnäləjē ˈbetər/</em>
@@ -52,44 +54,45 @@ const HomePage = ({ topics, totalPages }: HomePageProps) => {
                 <em>noun</em>
               </p>
               <ol className='list-inside list-decimal text-gray-800'>
-                <li className='mb-2'>
+                <li className='mb-2 text-lg'>
                   A person responsible for making decisions informed by an
                   up-to-date understanding of software industry trends.
                 </li>
-                <li>
+                <li className='mb-2 text-lg'>
                   Anyone who risks time or money based on the future popularity
                   of a particular software-related technology.
                 </li>
               </ol>
             </div>
-            <div className='w-full pl-4 md:w-1/2 md:pl-8'>
-              <p className='mb-2 text-2xl font-bold'>Are you one of us?</p>
-              <ul className='list-inside text-gray-800'>
-                <li className='mb-4'>
-                  <span className='font-semibold'>Engineering leaders:</span>{' '}
-                  Gain insights into the latest tools and technologies to help
-                  your team stay at the cutting edge.
-                </li>
-                <li className='mb-4'>
-                  <span className='font-semibold'>Technology investors:</span>{' '}
-                  Make informed investments based on data-driven insights into
-                  technology adoption and growth.
-                </li>
-                <li className='mb-4'>
-                  <span className='font-semibold'>
-                    Developer-focused marketers:
-                  </span>{' '}
-                  Understand industry trends to better target your messaging and
-                  engage your audience.
-                </li>
-                <li className='mb-4'>
-                  <span className='font-semibold'>
-                    Developer-focused product leaders:
-                  </span>{' '}
-                  Identify opportunities and prioritize features based on
-                  industry-wide trends and competitor analysis.
-                </li>
-              </ul>
+            <div className='w-full pt-16 pl-4 md:w-1/2 md:pl-8'>
+              <p className='mb-2 text-2xl font-semibold'>
+                Are you a technology bettor?
+              </p>
+              <p className='mb-2 pt-4 pb-2 text-lg'>
+                You might be if you're...
+              </p>
+              <div className='pl-4'>
+                <ul className='list-outside list-disc text-gray-800'>
+                  <li className='mb-4 text-lg'>
+                    An <span className='font-semibold'>technologist</span>{' '}
+                    trying decide what tools are worth learning or using in your
+                    stack.
+                  </li>
+                  <li className='mb-4 text-lg'>
+                    An <span className='font-semibold'>investor</span> looking
+                    to get data-driven about the developer infrastructure and
+                    comercial open source space.
+                  </li>
+                  <li className='mb-4 text-lg'>
+                    A{' '}
+                    <span className='font-semibold'>
+                      developer-focused marketer
+                    </span>{' '}
+                    or <span className='font-semibold'>product leader</span>{' '}
+                    tring to stay on top of trends that affect your customers.
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </section>
